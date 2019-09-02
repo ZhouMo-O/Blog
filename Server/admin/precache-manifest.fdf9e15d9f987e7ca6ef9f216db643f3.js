ca6b9d60a7e0c1408f8a04ef40b1038545a51814
five <1181649328@qq.com>
2019-09-02 13:11:21 +0800

    删除了一些素材

---
+++ Server/admin/precache-manifest.fdf9e15d9f987e7ca6ef9f216db643f3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "a782baa8633b1359f9686ffad17e0d76",
+    "url": "/fonts/fontello.a782baa8.woff"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "068ca2b316db98037bebdd1e4f1b9459",
+    "url": "/fonts/fontello.068ca2b3.ttf"
+  },
+  {
+    "revision": "08b84de5dd869b6d9d2d",
+    "url": "/css/chunk-vendors.e464e8a9.css"
+  },
+  {
+    "revision": "a23210a0bad811475255613b705e5087",
+    "url": "/img/codeCover.a23210a0.png"
+  },
+  {
+    "revision": "d805e4775e6201b010e14c0548614bb4",
+    "url": "/img/read.d805e477.svg"
+  },
+  {
+    "revision": "73f2266a479931e8cb24b096bfc0e2e9",
+    "url": "/img/like.73f2266a.svg"
+  },
+  {
+    "revision": "9b1f86091278cd5f9b5a81bece633f18",
+    "url": "/img/pinglun.9b1f8609.svg"
+  },
+  {
+    "revision": "8d4a4e6f7431d0d7fa92b1df20f38161",
+    "url": "/fonts/fontello.8d4a4e6f.woff2"
+  },
+  {
+    "revision": "e73a0647198cfe970de0f003be95cc51",
+    "url": "/fonts/fontello.e73a0647.eot"
+  },
+  {
+    "revision": "a582280b40f79a6c0f97",
+    "url": "/js/app.27252d41.js"
+  },
+  {
+    "revision": "08b84de5dd869b6d9d2d",
+    "url": "/js/chunk-vendors.92e404cb.js"
+  },
+  {
+    "revision": "9354499c2824248511adf85fdf8e4c37",
+    "url": "/img/fontello.9354499c.svg"
+  },
+  {
+    "revision": "3e641495c45c74c7caafb86323416724",
+    "url": "/img/edit.3e641495.svg"
+  },
+  {
+    "revision": "8f3de2ae84aab559aa828e06956c587f",
+    "url": "/img/del.8f3de2ae.svg"
+  },
+  {
+    "revision": "2305c02818c4b1af07d2e9d49d0abb4f",
+    "url": "/img/yes.2305c028.svg"
+  },
+  {
+    "revision": "a078b2a5b4958499ff41cca33ba6f313",
+    "url": "/img/no.a078b2a5.svg"
+  },
+  {
+    "revision": "5a507c1e75c7f8455de9021c94ac68e5",
+    "url": "/img/zaoshang.5a507c1e.jpg"
+  },
+  {
+    "revision": "77b68be242e72b8cb207f402b17de3a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a582280b40f79a6c0f97",
+    "url": "/css/app.fb814455.css"
+  }
+];
